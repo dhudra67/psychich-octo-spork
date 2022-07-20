bdad43dfc7d890e1762e2fefda4077ef0ac2ed8f
dhudra67 <muhammadfaizanali67@gmil.com>
2022-07-20 07:21:51 +0500

    initial commit

---
+++ madlibs.py
@@ -0,0 +1,11 @@
+var1 = input("Adjective: ")
+var2 = input("Worries: ")
+var3 = input("Adjective: ")
+
+
+
+
+
+madlibs = "you are a nice , good looking " + var1 + " but you have going throigh a lot " + var2  + " it is is a good " + var3
+
+print(madlibs)
